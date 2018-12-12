c47304b950d982170fbbd73348acdcac79c8be73
fengmushu <fengmushu@gmail.com>
2018-12-12 17:43:26 +0800

    update start.sh for h6

--- start.sh
+++ start.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-echo "Connect device in FEL mode and press <Enter>"
+BOARD=$1
+[ -n $BOARD ] || BOARD=h3
+
+echo "Connect '$BOARD' device in FEL mode and press <Enter>"
 read
 
-BOARD=h3
+case $BOARD in
+    h3)
+	sunxi-fel -p uboot $BOARD/u-boot-sunxi-with-spl.bin \
+		write 0x42000000 $BOARD/zImage \
+		write 0x43000000 $BOARD/script.bin \
+		write 0x43300000 $BOARD/uInitrd \
+		write 0x43100000 $BOARD/boot.scr
+	read
+    ;;
+    h6)
+	### write 0x4fe00000 initramfs.uimg ###
+	sunxi-fel -v -p spl $BOARD/sunxi-spl.bin \
+		write 0x104000 $BOARD/bl31.bin \
+		write 0x4a000000 $BOARD/u-boot.bin \
+		write 0x40080000 $BOARD/linux-failsafe \
+		write 0x4fa00000 $BOARD/dtb-failsafe \
+		write 0x4fc00000 $BOARD/boot.scr \
+		reset64 0x104000
+	read
+    ;;
+    *)
+	echo "unsupported platform $BOARD"
+    ;;
+esac
 
-sunxi-fel -p uboot $BOARD/u-boot-sunxi-with-spl.bin write 0x42000000 $BOARD/zImage write 0x43000000 $BOARD/script.bin write 0x43300000 $BOARD/uInitrd write 0x43100000 $BOARD/boot.scr
-read
